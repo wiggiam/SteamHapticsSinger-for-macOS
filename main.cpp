95a8cce1e34ce899f5a9b89019d0246c959ee1f7
William <william@Williams-MacBook-Air.local>
2026-05-15 09:39:47 +0100

    Fix dynamic channel array initialisation bugs for macOS compiler compatibility

--- main.cpp
+++ main.cpp
@@ -293,7 +293,7 @@ void playSong(SteamControllerInfos* controller,const ParamsStruct params){
 	sleep(1);
 
 	//This will contains the previous events accepted for each channel
-	MidiFileEvent_t acceptedEventPerChannel[channelCount] = {0};
+	MidiFileEvent_t acceptedEventPerChannel[channelCount]; memset(acceptedEventPerChannel, 0, sizeof(acceptedEventPerChannel));
 
 	//Get current time point, will be used to know elapsed time
 	std::chrono::steady_clock::time_point tOrigin = std::chrono::steady_clock::now();
@@ -306,7 +306,7 @@ void playSong(SteamControllerInfos* controller,const ParamsStruct params){
 		usleep(params.intervalUSec);
 
 		//This will contains the events to play
-		MidiFileEvent_t eventsToPlay[channelCount] = {NULL};
+		MidiFileEvent_t eventsToPlay[channelCount]; memset(eventsToPlay, 0, sizeof(eventsToPlay));
 
 		//We now need to play all events with tick < currentTime
 		long currentTick = MidiFile_getTickFromTime(midifile,timeElapsedSince(tOrigin));
